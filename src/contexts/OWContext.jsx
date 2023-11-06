dfec5914092ebd9bd4a9a3700afe19b4e9e2dddc
Davi Job <davi.job@hotmail.com>
2023-11-06 13:20:44 -0300

    Get current location on first render
    not fully tested yet

--- src/contexts/OWContext.jsx
+++ src/contexts/OWContext.jsx
@@ -2,36 +2,102 @@ import { createContext, useState, useEffect } from "react";
 
 export const OWContext = createContext();
 
-let lat = -23.5557714;
-let lon = -46.6395571;
-
 export const OWProvider = ({ children, setFetched }) => {
     const [OWData, setOWData] = useState({});
     const [local, setLocal] = useState({
-        lat: lat,
-        lon: lon,
+        lat: 0,
+        lon: 0,
     });
-    const [localName, setLocalName] = useState("São Paulo, SP");
+    const [localName, setLocalName] = useState("");
+
+    // get name, lat and lng from browser
+
+    const success = (position) => {
+        let lat = position.coords.latitude;
+        let lng = position.coords.longitude;
+
+        // get city and state from lat and lng
+        fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
+                import.meta.env.VITE_GOOGLE_PLACES_API_KEY
+            }`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                let city;
+                let state;
+                let dataLat = data.results[0].geometry.location.lat;
+                let dataLng = data.results[0].geometry.location.lng;
+
+                for (
+                    let i = 0;
+                    i < data.results[0].address_components.length;
+                    i++
+                ) {
+                    if (
+                        data.results[0].address_components[i].types[0] ===
+                        "administrative_area_level_2"
+                    ) {
+                        city = data.results[0].address_components[i].long_name;
+                    }
+                    if (
+                        data.results[0].address_components[i].types[0] ===
+                        "administrative_area_level_1"
+                    ) {
+                        state =
+                            data.results[0].address_components[i].short_name;
+                    }
+                }
+
+                setLocalName(`${city}, ${state}`);
+                setLocal({ lat: dataLat, lon: dataLng });
+
+                getOWData(dataLat, dataLng);
+            });
+    };
+
+    // set default location to São Paulo, SP
+
+    const error = () => {
+        setLocalName("São Paulo, SP");
+        setLocal({ lat: -23.533773, lon: -46.62529 });
+
+        alert("Default Location set to São Paulo, SP");
+    };
+
+    // get data in first render
 
     useEffect(() => {
-        getOWData();
+        const fetchData = async () => {
+            await getCurrentPlace();
+        };
+
+        fetchData();
     }, []);
 
+    // get data when local changes
+
     useEffect(() => {
-        getOWData();
+        getOWData(local.lat, local.lon);
     }, [local]);
 
-    const getOWData = async () => {
+    // get data from openweathermap.org
+
+    const getOWData = async (lat, lon) => {
         const response = await fetch(
-            `https://api.openweathermap.org/data/3.0/onecall?lat=${
-                local.lat
-            }&lon=${local.lon}&units=metric&appid=${
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${
                 import.meta.env.VITE_OPEN_WEATHER_API_KEY
             }`
         );
         const jsonData = await response.json();
         setFetched(true);
         setOWData(jsonData);
+    };
+
+    // get current place from browser
+
+    const getCurrentPlace = () => {
+        navigator.geolocation.getCurrentPosition(success, error);
     };
 
     return (
